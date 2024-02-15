f95da1628c05657ee479a1156854212428c0faca
emerix <ba.sel311103@protonmail.com>
2024-02-15 19:48:27 +0300

    making the triangle move left and right

--- OpenGLApplication/main.cpp
+++ OpenGLApplication/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -9,18 +10,27 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.00055f;
+
 
 //Vertex Shader
-static const char* vShader = "										\n\
-#version 330														\n\
-																	\n\
-layout (location =0) in vec3 pos;									\n\
-																	\n\
-void main()															\n\
-{																	\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		\n\
-}																	\n\
+static const char* vShader = "													\n\
+#version 330																	\n\
+																				\n\
+layout (location =0) in vec3 pos;												\n\
+																				\n\
+uniform float xMove;															\n\
+																				\n\
+																				\n\
+void main()																		\n\
+{																				\n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);			\n\
+}																				\n\
 ";	
 
 // Fragment Shader
@@ -120,6 +130,9 @@ void CompileShaders() {
 		return;
 	}
 
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
+
 }
 
 int main()
@@ -179,11 +192,26 @@ int main()
 		// Get + Handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
