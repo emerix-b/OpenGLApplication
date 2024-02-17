8e8463f6b1a130d7ac589cb4cc05d6479eff92e9
emerix <ba.sel311103@protonmail.com>
2024-02-17 13:14:24 +0300

    Scaling the triangle

--- OpenGLApplication/main.cpp
+++ OpenGLApplication/main.cpp
@@ -22,6 +22,11 @@ float triIncrement = 0.0005f;
 
 float curAngle = 0.0f;
 
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
+
 //Vertex Shader
 static const char* vShader = "													\n\
 #version 330																	\n\
@@ -33,7 +38,7 @@ uniform mat4 model;																\n\
 																				\n\
 void main()																		\n\
 {																				\n\
-	gl_Position = model * vec4(0.4 * pos.x, -0.4 * pos.y, pos.z, 2.0);			\n\
+	gl_Position = model * vec4(pos, 1.0);										\n\
 }																				\n\
 ";	
 
@@ -215,6 +220,20 @@ int main()
 			curAngle -= 360;
 		}
 
+		if (sizeDirection)
+		{
+			curSize += 0.0001f;
+		}
+		else
+		{
+			curSize -= 0.0001f;
+		}
+
+		if (curSize >= maxSize || curSize <= minSize)
+		{
+			sizeDirection = !sizeDirection;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -222,8 +241,12 @@ int main()
 		glUseProgram(shader);
 
 		glm::mat4 model;
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
 		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
